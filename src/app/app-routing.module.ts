91ed783a760449f9456dd9a18f73a4af53c089c6
Guilherme <francaguilherme27@gmail.com>
2024-05-16 20:10:23 -0300

    feat: Criating the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { EasyComponent } from './main/easy/easy.component';
+import { MediumComponent } from './main/medium/medium.component';
+import { HardComponent } from './main/hard/hard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'facil', pathMatch: 'full' },
+  {
+    path: '', component: MainComponent, children: [
+      { path: 'facil', component: EasyComponent },
+      { path: 'medio', component: MediumComponent },
+      { path: 'dificil', component: HardComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

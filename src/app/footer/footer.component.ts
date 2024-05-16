278005004f1ed317251397f87137205249a27f06
Guilherme <francaguilherme27@gmail.com>
2024-05-16 16:17:49 -0300

    feat: My social media links

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  main_links: any[] = [
+    {
+      link: 'https://www.linkedin.com/in/guilherme-fran%C3%A7a-da-silva-4756a8155',
+      icon: 'https://guilhermefdsilva.github.io/read-db-myPortfolio/logos/linkedin.svg',
+      description: 'Linkedin'
+    },
+    {
+      link: 'https://github.com/GuilhermeFdSilva',
+      icon: 'https://guilhermefdsilva.github.io/read-db-myPortfolio/logos/gitHub.svg',
+      description: 'GitHub'
+    },
+  ]
 }

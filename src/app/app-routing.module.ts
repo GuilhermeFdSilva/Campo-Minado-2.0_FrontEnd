845e09065ad7af864188add6381e79032a876bbc
Guilherme <francaguilherme27@gmail.com>
2024-05-21 16:48:47 -0300

    remove: Medium component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
 import { EasyComponent } from './main/easy/easy.component';
-import { MediumComponent } from './main/medium/medium.component';
 import { HardComponent } from './main/hard/hard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'facil', pathMatch: 'full' },
   { path: 'facil', component: EasyComponent },
-  { path: 'medio', component: MediumComponent },
   { path: 'dificil', component: HardComponent }
 ];
 

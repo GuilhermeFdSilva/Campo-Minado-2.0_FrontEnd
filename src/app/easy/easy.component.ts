4e3f6505e26fc79895f96e5d987432e0ad6b39e0
Guilherme <francaguilherme27@gmail.com>
2024-06-25 18:52:06 -0300

    feat: Easy component creation

---
+++ src/app/easy/easy.component.ts
@@ -0,0 +1,13 @@
+import { GameDecoderService } from 'src/assets/services/game-classes/game-decoder.service';
+import { Board } from '../../assets/services/game-classes/board/board';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Field } from 'src/assets/services/game-classes/field/field';
+
+@Component({
+  selector: 'app-easy',
+  templateUrl: './easy.component.html',
+  styleUrls: ['./easy.component.scss']
+})
+export class EasyComponent implements OnInit {
+
+}
